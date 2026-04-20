d61669c8a678106971d8907ae96dae05bbf1e460
cutebard <jacksonchartman98@gmail.com>
2026-04-19 20:38:54 -0700

    Create 4qahubfe854pbnmx1g45fgnhwbjer.js

---
+++ website/assets/js/secret/4qahubfe854pbnmx1g45fgnhwbjer.js
@@ -0,0 +1,15 @@
+let a = document.getElementById("r");
+let d = null;
+
+
+
+// entertainment purposes only.
+a.addEventListener("click", () => {
+    d = prompt("type in away.");
+    if (d == "T3") {
+        window.URLSearchParams.apply("https://cdn.discordapp.com/attachments/1472416389007540434/1490407196901769287/20260404-2351-22.8947175.mp4");
+    } else if (d == "TP") {
+        window.URLSearchParams.apply("https://cdn.discordapp.com/attachments/1472416389007540434/1494170098045423656/My_Plotagon_Movie_1_1_1_1.mp4");
+    }
+}
+);
