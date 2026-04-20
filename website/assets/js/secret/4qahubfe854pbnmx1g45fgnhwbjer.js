30778159da1ff955f7c3a80f39063c9d19c23af0
cutebard <jacksonchartman98@gmail.com>
2026-04-19 20:48:19 -0700

    Update 4qahubfe854pbnmx1g45fgnhwbjer.js

--- website/assets/js/secret/4qahubfe854pbnmx1g45fgnhwbjer.js
+++ website/assets/js/secret/4qahubfe854pbnmx1g45fgnhwbjer.js
@@ -4,9 +4,9 @@ let d = null;
 a.addEventListener("click", () => {
     d = prompt("type in away.");
     if (d == "T3") {
-       new window.URLSearchParams.apply("https://cdn.discordapp.com/attachments/1472416389007540434/1490407196901769287/20260404-2351-22.8947175.mp4");
+       window.location.assign("https://cdn.discordapp.com/attachments/1472416389007540434/1490407196901769287/20260404-2351-22.8947175.mp4");
     } else if (d == "TP") {
-        new window.URLSearchParams.apply("https://cdn.discordapp.com/attachments/1472416389007540434/1494170098045423656/My_Plotagon_Movie_1_1_1_1.mp4");
+        window.location.assign("https://cdn.discordapp.com/attachments/1472416389007540434/1494170098045423656/My_Plotagon_Movie_1_1_1_1.mp4");
     }
 }
 );
