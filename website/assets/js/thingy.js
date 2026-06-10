69031ac6061c3fdc970341790e57d32ced9b3199
cutebard <jacksonchartman98@gmail.com>
2026-06-10 16:24:12 -0700

    s

---
+++ website/assets/js/thingy.js
@@ -0,0 +1,10 @@
+import { init, display, image, Sound, draw, Color, Rect, key, K_LEFT, K_RIGHT } from '/website/pygame.js';
+
+var idkwhatthiswindowshouldbecalled = display.set_mode([500, 500])
+
+var frames = 0
+
+idkwhatthiswindowshouldbecalled.fill(new Color("GREEN"))
+while (frames < 100) {
+    draw.Rect()
+}
